5128f9e0ab850dae7d6b5ea8061551aa7fe1c0a3
Logan McAnsh <logan@mcan.sh>
2017-08-30 19:21:23 -0400

    add hyper updateChannel for v1.4.0+

--- home/.hyper.js
+++ home/.hyper.js
@@ -131,5 +131,7 @@ module.exports = {
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
   // to load it and avoid it being `npm install`ed
-  localPlugins: []
+  localPlugins: [],
+
+  updateChannel: 'canary'
 };
