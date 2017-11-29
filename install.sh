ecba26c9088610b054e5c92ffca0d93d3f4dfffd
Logan McAnsh <logan@mcan.sh>
2017-11-29 10:45:40 -0500

    update install script

--- install.sh
+++ install.sh
@@ -1,22 +1,14 @@
 #!/usr/bin/env bash
 
-# Set up logging environment
-LOG_FILE='/Users/'$(logname)'/Desktop/install-log.log'
-
-exec &> >(tee -a "$LOG_FILE")
-echo "Logging to" $LOG_FILE
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
 # Set HostName/ComputerName/LocalHostName
-sudo -u $SUDO_USER scutil --set ComputerName "🚀😺"
-sudo -u $SUDO_USER scutil --set LocalHostName "mcansh"
-sudo -u $SUDO_USER scutil --set HostName "mcansh"
-echo "Host name is set to $(hostname)"
+sudo -u $USER scutil --set ComputerName "🚀😺"
+sudo -u $USER scutil --set LocalHostName "mcansh"
+sudo -u $USER scutil --set HostName "mcansh"
+echo "> 1/4 Host name is set to $(hostname)"
 
 # Mac configuration
-echo "Setting up computer configuration"
-sudo -u $SUDO_USER systemsetup -setcomputersleep 2 # Computer sleeps after 2 minutes
+echo "> 2/4 Setting up computer configuration"
+sudo -u $USER systemsetup -setcomputersleep 2 # Computer sleeps after 2 minutes
 defaults write com.apple.screensaver askForPassword 1 # Force password entry after sleep
 defaults write /Library/Preferences/com.apple.loginwindow LoginwindowText "This computer belongs to Logan McAnsh" # add a message to the login screen
 defaults write com.apple.Safari AutoOpenSafeDownloads -bool false # stop safari from opening downloaded files automatically
@@ -31,59 +23,44 @@ killall -KILL SystemUIServer # restart menubar
 
 # Check if FileValut is on
 if [ "$(fdesetup status)" == "FileVault is On." ]; then
-  echo "Disk encryption is already enabled. 🔥"
+  echo "> 3/4 Disk encryption is already enabled. 🔥"
 fi
 
 if [ "$(fdesetup status)" == "FileVault is Off." ]; then
   echo "Disk encryption not enabled. Enabling now..."
   fdesetup enable
-  echo "Disk encryption is now enabled. 💯🔥"
+  echo "> 3/4 Disk encryption is now enabled. 💯🔥"
 fi
 
 # Install Brew
-sudo -u $SUDO_USER /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+sudo -u $USER /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Install ZSH
-sudo -u $SUDO_USER brew install zsh zsh-completions
+sudo -u $USER brew install zsh zsh-completions
 
 # Install oh my ZSH
-sudo -u $SUDO_USER /usr/bin/ruby -e "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sudo -u $USER /usr/bin/ruby -e "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Install Zeit's theme (usage is in .zshrc)
-sudo -u $SUDO_USER curl https://raw.githubusercontent.com/zeit/zeit.zsh-theme/master/zeit.zsh-theme -Lo ~/.oh-my-zsh/themes/zeit.zsh-theme
+sudo -u $USER curl https://raw.githubusercontent.com/zeit/zeit.zsh-theme/master/zeit.zsh-theme -Lo ~/.oh-my-zsh/themes/zeit.zsh-theme
 
 # Setup Software
-echo "Installing software from cask... (may take a bit if your internet is shit)"
-cask_apps=('atom-beta' 'codekit' 'cyberduck' 'handbrake' 'hyper' 'transmission-nightly' 'google-chrome' 'google-chrome-canary' 'zoomus' 'whiskey' 'slack')
-for i in "${cask_apps[@]}"
-do
-  brew cask install "${cask_apps[@]}"
-done
+sudo -u $USER brew install mas # We will need this in a bit for installing MAS
+sudo -u $USER brew install gnupg21 android-platform-tools gifify hub pinentry-mac thefuck
+sudo -u $USER brew tap caskroom/cask
+sudo -u $USER brew tap caskroom/versions
+sudo -u $USER brew tap homebrew/bundle
+sudo -u $USER brew tap homebrew/core
+sudo -u $USER brew tap homebrew/dupes
+sudo -u $USER brew cask install Caskroom/cask/$cask_apps
+sudo -u $USER brew bundle
 
-sudo -u $SUDO_USER brew install mas # We will need this in a bit for installing MAS
-sudo -u $SUDO_USER brew install gnupg21 android-platform-tools gifify hub pinentry-mac thefuck
-sudo -u $SUDO_USER brew tap caskroom/cask
-sudo -u $SUDO_USER brew tap caskroom/versions
-sudo -u $SUDO_USER brew tap homebrew/bundle
-sudo -u $SUDO_USER brew tap homebrew/core
-sudo -u $SUDO_USER brew tap homebrew/dupes
-sudo -u $SUDO_USER brew cask install Caskroom/cask/$cask_apps
-sudo -u $SUDO_USER brew bundle
-
-# Install Atom Packages
-echo "Installing Atom Packages... This will take a few..."
-
-if [ apm ]; then
-  apm install --packages-file ~/.homesick/repos/dotfiles/atom-list.txt
-else if [ apm-beta ]; then
-  apm-beta install --packages-file ~/.homesick/repos/dotfiles/atom-list.txt
-else
-  echo "Couldn't find apm or apm-beta... You'll need to manually install your atom packages 😭"
-fi
-
+echo "> 4/4 Installing software from cask... (may take a bit if your internet is shit)"
+cask_apps=('1password' 'handbrake' 'codekit' 'hyper' 'dropbox' 'slack-beta' 'firefoxnightly' 'visual-studio-code-insiders' 'google-chrome-canary' 'whiskey' 'google-chrome-dev' 'zoomus')
+brew cask install $cask_apps
 
 # Mac Software Update Check and install updates
 softwareupdate -ia --verbose
 
 # WE OUT
-echo "Welcome to your newly configured MacBook"
+echo "> Welcome to your newly configured MacBook"
