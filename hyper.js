5107c0cd8be4a74b576d6b89dc2268aee533a89c
Logan McAnsh <logan@mcan.sh>
2018-03-14 19:21:49 -0400

    set hyper font to operator mono

--- hyper.js
+++ hyper.js
@@ -13,7 +13,7 @@ module.exports = {
 
     // font family with optional fallbacks
     fontFamily:
-      'SFMono-Medium, Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+      '"Operator Mono", SFMono-Medium, Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
 
     // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
     cursorColor: 'rgba(248,28,229,0.8)',
