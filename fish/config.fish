aba30f60e04635462e9a6d6ca9659df7cd560a12
Logan McAnsh <logan@mcan.sh>
2019-04-18 11:12:11 -0400

    remove settings for removed plugins
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- fish/config.fish
+++ fish/config.fish
@@ -9,3 +9,13 @@ thefuck --alias | source
 export PATH="$PATH:$HOME/.my_bin";
 export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
 rvm default
+
+# tabtab source for serverless package
+# uninstall by removing these lines or running `tabtab uninstall serverless`
+[ -f /Users/loganmcansh/.config/yarn/global/node_modules/tabtab/.completions/serverless.fish ]; and . /Users/loganmcansh/.config/yarn/global/node_modules/tabtab/.completions/serverless.fish
+# tabtab source for sls package
+# uninstall by removing these lines or running `tabtab uninstall sls`
+[ -f /Users/loganmcansh/.config/yarn/global/node_modules/tabtab/.completions/sls.fish ]; and . /Users/loganmcansh/.config/yarn/global/node_modules/tabtab/.completions/sls.fish
+# tabtab source for slss package
+# uninstall by removing these lines or running `tabtab uninstall slss`
+[ -f /Users/loganmcansh/.config/yarn/global/node_modules/tabtab/.completions/slss.fish ]; and . /Users/loganmcansh/.config/yarn/global/node_modules/tabtab/.completions/slss.fish
