2342e6b6b2db5c6c228e293354a26aee79320395
Logan McAnsh <logan@mcan.sh>
2019-08-12 13:38:36 -0400

    cspell and stuff

--- .hyper.js
+++ .hyper.js
@@ -141,7 +141,9 @@ module.exports = {
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
   // to load it and avoid it being `npm install`ed
-  localPlugins: [],
+  localPlugins: [
+    // 'hyper-light-owl'
+  ],
 
   keymaps: {
     // Example
