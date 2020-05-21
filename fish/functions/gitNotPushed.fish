197b2399eadbfe1edd5f5955ce35daba89730b85
Logan McAnsh <logan@mcan.sh>
2020-05-21 19:03:13 -0400

    chore: format gitNotPushed log
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- fish/functions/gitNotPushed.fish
+++ fish/functions/gitNotPushed.fish
@@ -2,7 +2,7 @@ function gitNotPushed
     set current_branch (git rev-parse --abbrev-ref HEAD)
     set count (git rev-list origin/$current_branch...$current_branch --count)
     if test $count -gt 0
-        git log origin/$current_branch..$current_branch
+        git log --pretty=format:"%C(yellow)%h\\ %ad%Cred%d\\ %Creset%s%Cblue\\ [%cn]" --decorate --date=relative origin/$current_branch..$current_branch
     else
         echo "origin/$current_branch is up to date with $current_branch"
     end
