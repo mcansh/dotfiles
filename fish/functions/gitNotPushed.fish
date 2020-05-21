6412f5d47c58cccf5a28b0174d7f58d00f873456
Logan McAnsh <logan@mcan.sh>
2020-05-21 19:01:55 -0400

    chore: fix gitNotPushed
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- fish/functions/gitNotPushed.fish
+++ fish/functions/gitNotPushed.fish
@@ -1,10 +1,9 @@
 function gitNotPushed
-  current_branch="(git rev-parse --abbrev-ref HEAD)"
-  count="(git rev-list origin/(current_branch)...(current_branch) --count)"
-  if (($count > 0))
-  then
-    git log origin/(current_branch)..(current_branch)
-  else
-    echo "origin/$current_branch is up to date with $current_branch"
-  fi
+    set current_branch (git rev-parse --abbrev-ref HEAD)
+    set count (git rev-list origin/$current_branch...$current_branch --count)
+    if test $count -gt 0
+        git log origin/$current_branch..$current_branch
+    else
+        echo "origin/$current_branch is up to date with $current_branch"
+    end
 end
