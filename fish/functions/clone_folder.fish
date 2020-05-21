8a8b3c0d62ca6dff2a3ca23f775c8d36f5dce6b2
Logan McAnsh <logan@mcan.sh>
2020-05-21 19:01:38 -0400

    chore: remove echoes
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- fish/functions/clone_folder.fish
+++ fish/functions/clone_folder.fish
@@ -1,8 +1,5 @@
 function clone_folder
     set folder (echo "$argv" | sed -e 's/.*\///g')
     set repo (string replace -r "/tree/master" "/trunk" $argv)
-    echo $argv
-    echo $folder
-    echo $repo
     svn checkout $repo
 end
