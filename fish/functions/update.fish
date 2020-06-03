dd5d06028c0151c48bf2ea6f596d82b809df55da
Logan McAnsh <logan@mcan.sh>
2020-06-02 21:37:45 -0400

    fix(update): make macOS update work with or without args
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- fish/functions/update.fish
+++ fish/functions/update.fish
@@ -1,9 +1,9 @@
 function update
-  echo "▲ running macOS update"
-  if test $argv = '--restart'
-    echo '▲ using "--restart" requires root privileges, I don\'t make the rules'
-    sudo softwareupdate -ia --verbose --restart
-  else
-    softwareupdate -ia --verbose
-  end
+    echo "▲ running macOS update"
+    if test (count $argv) -eq 1 -a "$argv[1]" = "--restart"
+        echo '▲ using "--restart" requires root privileges, I don\'t make the rules'
+        sudo softwareupdate -ia --verbose --restart
+    else
+        softwareupdate -ia --verbose
+    end
 end
