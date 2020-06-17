4b753fff9085872ec337b3dc58221f04f2f49d57
Logan McAnsh <logan@mcan.sh>
2020-06-17 12:37:51 -0400

    chore(brewup): update brew casks
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- fish/functions/brewup.fish
+++ fish/functions/brewup.fish
@@ -1,13 +1,15 @@
 function brewup
-  echo '🍺 1/5 Updating Homebrew 📦'
-  brew update
-  echo '🍺 2/5 Checking Homebrew for issues ⛔️'
-  brew doctor
-  echo '🍺 3/5 Getting a list of oudated packages 📜'
-  brew outdated
-  echo '🍺 4/5 Upgrading packages 🚚'
-  brew upgrade
-  echo '🍺 5/5 Cleaning up 🚮'
-  brew cleanup
-  echo '🍺 Done. 🎉'
+    echo '🍺 1/6 Updating Homebrew 📦'
+    brew update
+    echo '🍺 2/6 Checking Homebrew for issues ⛔️'
+    brew doctor
+    echo '🍺 3/6 Getting a list of oudated packages 📜'
+    brew outdated
+    echo '🍺 4/6 Upgrading packages 🚚'
+    brew upgrade
+    echo '🍺 5/6 Cleaning up 🚮'
+    brew cleanup
+    echo '🍺 6/6 Updating Brew Casks'
+    brew cask upgrade
+    echo '🍺 Done. 🎉'
 end
