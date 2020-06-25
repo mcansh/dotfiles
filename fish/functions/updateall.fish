f06fe452cad8e17304b32afd30a14f34712d8af4
Logan McAnsh <logan@mcan.sh>
2020-06-25 10:51:57 -0400

    chore(updateall): update --restart check

--- fish/functions/updateall.fish
+++ fish/functions/updateall.fish
@@ -1,19 +1,19 @@
 # update everything
 function updateall
-  echo '▲ [1/5] Running Homebrew update script 🍺'
-  brewup
-  echo '▲ [2/5] Updating Rubygems 💎'
-  gem update
-  gem update --system
-  echo '▲ [3/5] Running Yarn Global Upgrade 🧶'
-  yarn global upgrade
-  echo '▲ [4/5] Updating Apps from MAS 🍎'
-  mas outdated
-  mas upgrade
-  echo '▲ [5/5] Running macOS Upgrade 🍏'
-  if test $argv = '--restart'
-    update --restart
-  else
-    update
-  end
+    echo '▲ [1/5] Running Homebrew update script 🍺'
+    brewup
+    echo '▲ [2/5] Updating Rubygems 💎'
+    gem update
+    gem update --system
+    echo '▲ [3/5] Running Yarn Global Upgrade 🧶'
+    yarn global upgrade
+    echo '▲ [4/5] Updating Apps from MAS 🍎'
+    mas outdated
+    mas upgrade
+    echo '▲ [5/5] Running macOS Upgrade 🍏'
+    if test (count $argv) -eq 1 -a "$argv[1]" = "--restart"
+        update --restart
+    else
+        update
+    end
 end
