7549b42f437016b6c390e2366900e7b5b43a4b76
Logan McAnsh <logan@mcan.sh>
2022-07-14 18:59:34 -0400

    chore: update finicky config
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- .finicky.js
+++ .finicky.js
@@ -5,23 +5,39 @@ module.exports = {
   defaultBrowser: "Safari",
   options: {
     hideIcon: true,
+    logRequests: true,
   },
   rewrite: [
     // Replace domain of urls to amazon.com with smile.amazon.com
     {
       match: finicky.matchDomains(["www.amazon.com", "amazon.com"]),
-      url: ({ url }) => ({ ...url, host: "smile.amazon.com" }),
+      url: ({ urlString }) => {
+        return { ...url, host: "smile.amazon.com" };
+      },
     },
-    // Remove all marketing/tracking information from urls
     {
-      match: ({ url }) => url.search.includes("utm_"),
-      url({ url }) {
+      // Remove all marketing/tracking information from urls
+      // Execute rewrite on all incoming urls to make this example easier to understand
+      match: () => true,
+      url: ({ url }) => {
+        const removeKeysStartingWith = ["utm_", "uta_"]; // Remove all query parameters beginning with these strings
+        const removeKeys = ["fbclid", "gclid"]; // Remove all query parameters matching these keys
+
         const search = url.search
           .split("&")
-          .filter((part) => !part.startsWith("utm_"));
+          .map((parameter) => parameter.split("="))
+          .filter(([key]) => {
+            return !removeKeysStartingWith.some((startingWith) =>
+              key.startsWith(startingWith)
+            );
+          })
+          .filter(([key]) => {
+            return !removeKeys.some((removeKey) => key === removeKey);
+          });
+
         return {
           ...url,
-          search: search.join("&"),
+          search: search.map((parameter) => parameter.join("=")).join("&"),
         };
       },
     },
